5a9a42b968461ca945f8384bf562946e486653fc
Andrii Trebukhin <andrewtrebuhin@gmail.com>
2024-03-27 17:21:32 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,44 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+[
+  {
+    address: '74.12.42.44',
+    location: {
+      latitude: 43.70317077636719,
+      longitude: -79.51219177246094,
+      country: 'Canada',
+      country_code: 'CA',
+      state: 'Ontario',
+      state_code: 'ON',
+      city: 'Toronto',
+      postal_code: 'M3H 6A7'
+    }
+  },
+  {
+    address: '70.12.45.47',
+    location: {
+      latitude: 39.10620880126953,
+      longitude: -94.57328033447266,
+      country: 'United States',
+      country_code: 'US',
+      state: 'Missouri',
+      state_code: 'MO',
+      city: 'Kansas City',
+      postal_code: '64106'
+    }
+  },
+  {
+    address: '66.183.83.48',
+    location: {
+      latitude: 49.180938720703125,
+      longitude: -122.81128692626953,
+      country: 'Canada',
+      country_code: 'CA',
+      state: 'British Columbia',
+      state_code: 'BC',
+      city: 'Surrey',
+      postal_code: 'V3R 0A1'
+    }
+  }
+].each do |attrs|
+  location = Location.find_or_create_by(attrs[:location])
+  Addressable.find_or_create_by(address: attrs[:address], location_id: location.id)
+end
